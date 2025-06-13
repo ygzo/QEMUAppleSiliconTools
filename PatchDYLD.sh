17a986771ee0716d04e0904d05f17c00310c81d5
Visual Ehrmanntraut <30368284+VisualEhrmanntraut@users.noreply.github.com>
2025-06-13 13:52:34 +0300

    fix: DYLD patch portability improvements

--- PatchDYLD.sh
+++ PatchDYLD.sh
@@ -8,26 +8,23 @@ if [ ! -f dyld_shared_cache_arm64e.orig ]; then
 fi
 
 echo "Restoring original DYLD..."
-cp dyld_shared_cache_arm64e.orig dyld_shared_cache_arm64e
+dd if=dyld_shared_cache_arm64e.orig of=/Volumes/System/System/Library/Caches/com.apple.dyld/dyld_shared_cache_arm64e
 
 echo "Applying CoreImage patches..."
 # Force `wrapGLIsUsable` to return false
-printf '\x00\x00\x80\xD2' | dd bs=1 count=4 seek=0x1E8F704 conv=notrunc of=dyld_shared_cache_arm64e
-printf '\xC0\x03\x5F\xD6' | dd bs=1 count=4 seek=0x1E8F708 conv=notrunc of=dyld_shared_cache_arm64e
+printf '\x00\x00\x80\xD2' | dd bs=1 count=4 seek=32044804 conv=notrunc of=/Volumes/System/System/Library/Caches/com.apple.dyld/dyld_shared_cache_arm64e
+printf '\xC0\x03\x5F\xD6' | dd bs=1 count=4 seek=32044808 conv=notrunc of=/Volumes/System/System/Library/Caches/com.apple.dyld/dyld_shared_cache_arm64e
 
 echo "Applying QuartzCore patches..."
 # Fix `CA::OGL::AsynchronousDispatcher::renderer` to use software context
-printf '\x1F\x20\x03\xD5' | dd bs=1 count=4 seek=0x81435F0 conv=notrunc of=dyld_shared_cache_arm64e
-printf '\x1F\x20\x03\xD5' | dd bs=1 count=4 seek=0x81435F4 conv=notrunc of=dyld_shared_cache_arm64e
-printf '\x1F\x20\x03\xD5' | dd bs=1 count=4 seek=0x81435F8 conv=notrunc of=dyld_shared_cache_arm64e
-printf '\xDD\x18\x02\x94' | dd bs=1 count=4 seek=0x814360C conv=notrunc of=dyld_shared_cache_arm64e
-printf '\x1F\x20\x03\xD5' | dd bs=1 count=4 seek=0x8143660 conv=notrunc of=dyld_shared_cache_arm64e
-printf '\x1F\x20\x03\xD5' | dd bs=1 count=4 seek=0x8143664 conv=notrunc of=dyld_shared_cache_arm64e
+printf '\x1F\x20\x03\xD5' | dd bs=1 count=4 seek=135542256 conv=notrunc of=/Volumes/System/System/Library/Caches/com.apple.dyld/dyld_shared_cache_arm64e
+printf '\x1F\x20\x03\xD5' | dd bs=1 count=4 seek=135542260 conv=notrunc of=/Volumes/System/System/Library/Caches/com.apple.dyld/dyld_shared_cache_arm64e
+printf '\x1F\x20\x03\xD5' | dd bs=1 count=4 seek=135542264 conv=notrunc of=/Volumes/System/System/Library/Caches/com.apple.dyld/dyld_shared_cache_arm64e
+printf '\xDD\x18\x02\x94' | dd bs=1 count=4 seek=135542284 conv=notrunc of=/Volumes/System/System/Library/Caches/com.apple.dyld/dyld_shared_cache_arm64e
+printf '\x1F\x20\x03\xD5' | dd bs=1 count=4 seek=135542368 conv=notrunc of=/Volumes/System/System/Library/Caches/com.apple.dyld/dyld_shared_cache_arm64e
+printf '\x1F\x20\x03\xD5' | dd bs=1 count=4 seek=135542372 conv=notrunc of=/Volumes/System/System/Library/Caches/com.apple.dyld/dyld_shared_cache_arm64e
 
 echo "Applying SpringBoardFoundation.framework patches..."
 # Force `shouldUseXPCForRendering` to return true
-printf '\x20\x00\x80\xD2' | dd bs=1 count=4 seek=0x15E94F18 conv=notrunc of=dyld_shared_cache_arm64e
-printf '\xC0\x03\x5F\xD6' | dd bs=1 count=4 seek=0x15E94F1C conv=notrunc of=dyld_shared_cache_arm64e
-
-echo "Moving patched DYLD..."
-mv dyld_shared_cache_arm64e /Volumes/System/System/Library/Caches/com.apple.dyld/dyld_shared_cache_arm64e
+printf '\x20\x00\x80\xD2' | dd bs=1 count=4 seek=367611672 conv=notrunc of=/Volumes/System/System/Library/Caches/com.apple.dyld/dyld_shared_cache_arm64e
+printf '\xC0\x03\x5F\xD6' | dd bs=1 count=4 seek=367611676 conv=notrunc of=/Volumes/System/System/Library/Caches/com.apple.dyld/dyld_shared_cache_arm64e
